d0b5a6ea32d70eef63e6d8e0c1ea7e1c456d2a13
Marcus <marcusventin@gmail.com>
2021-12-15 14:31:22 +0000

    update

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,17 @@
 require_relative "bike"
 
 class DockingStation
+  attr_reader :bike
   def release_bike
-    Bike.new
+    @bike
+    end
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  def empty?
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,15 @@
 require "docking_station"
 
 describe DockingStation do
+  
+  describe "#release_bike" do
+   it "releases a bike" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
+    end
+  end
+
   describe "responds to release_bike" do
     it { is_expected.to respond_to :release_bike }
   end
@@ -9,4 +18,25 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it "docks something" do
+   bike = Bike.new
+   expect(subject.dock(bike)).to eq bike
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  describe "responds to empty?" do
+    it { is_expected.to respond_to :empty?}
+  end
+
+  it "raises error when release_bike from empty station" do
+    subject.empty?
+    expect(subject.release_bike).to raise_error
+  end
+
 end

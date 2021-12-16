e43997d50308c2878199301ba72c742a535aa076
Michael Zwickler <zwickler.michael@googlemail.com>
2021-12-16 14:56:40 +0100

    indentation improved for better readibility

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -27,32 +27,24 @@ RSpec.describe DockingStation do
     end
   end
 
-describe '#dock' do
-  context 'valid request, capacity not at limit' do
-    it "docks a bike" do
-      bike = Bike.new
-      expect(subject.dock(bike)[0]).to eq bike
-     end
-  end
-  context 'valid request, capacity not at limit' do
-    it "returns docked bikes" do
-      bike = Bike.new
-      expect(subject.dock(bike)[0]).to eq bike
+  describe '#dock' do
+    context 'valid request, capacity not at limit' do
+      it "docks a bike" do
+        bike = Bike.new
+        expect(subject.dock(bike)[0]).to eq bike
+      end
+    end
+    context 'valid request, capacity not at limit' do
+      it "returns docked bikes" do
+        bike = Bike.new
+        expect(subject.dock(bike)[0]).to eq bike
+      end
+    end
+    context 'invalid request, capacity at limit' do
+      it 'doesnt exceed dockingstation capacity' do
+        DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
+        expect{subject.dock(Bike.new)}.to raise_error('No space left')
+      end
     end
   end
-  context 'invalid request, capacity at limit' do
-    it 'doesnt exceed dockingstation capacity' do
-      DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
-      expect{subject.dock(Bike.new)}.to raise_error('No space left')
-    end
-  end
-end
-
-
-
-  
-
-
-
-
 end
